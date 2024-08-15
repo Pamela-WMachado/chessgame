9d552ee06c26016cdab062b1dfc6b2414d497e96
Pamela-WMachado <pamela.machado@capgemini.com>
2024-08-15 10:57:59 -0300

    1st class: Position

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import boardgame.Position;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Chess game!!");
+        Position pos = new Position(3, 9);
+
+        System.out.println(pos);
     }
 }
