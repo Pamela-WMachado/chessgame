86ded2e291c54ad9b66b5bb8f81226ba82aa8ac3
Pamela-WMachado <pamela.machado@capgemini.com>
2024-08-27 17:04:15 -0300

    Introduction to moving pieces

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         ChessMatch match = new ChessMatch();
-        UI.printBoard(match.getPieces());
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            UI.printBoard(match.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = match.performChessMove(source, target);
+        }
+
 
     }
 }
